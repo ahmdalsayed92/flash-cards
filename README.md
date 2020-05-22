c3b63358092d64d85c56e88113d6914b8ce28dd0
Ahmad-Elsayed <ahmad.elsayed@ibm.com>
2020-05-22 16:43:41 +0200

    flash cards

---
+++ README.md
@@ -0,0 +1,12 @@
+# About
+
+This is the third project for Udacity mobile flashcard work. You could manage decks and cards in it to answer questions at any time with your phone.
+
+# How to start project
+
+Run
+
+```
+npm install
+npm start
+```
---
+++ utils/colors.js
@@ -0,0 +1,9 @@
+/**
+ * @description color palette for our app
+ */
+export const white = "#fff";
+export const primary = "#669fbb";
+export const success = "#47a447";
+export const danger = "#d2322d";
+export const action = "#01608f";
+export const secondary = "#123456";
